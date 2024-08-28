cd13ed1aee869c3ab44d3b62d0ceabce2062d6bc
Mathias WOLFF <mathias@celea.org>
2024-08-28 15:56:52 +0200

    sector group name is now sectorname_jobid

--- wazo_koala/services.py
+++ wazo_koala/services.py
@@ -46,10 +46,10 @@ class KoalaService(object):
             if str(group['label']) == group_name:
                 return group['id']
                 
-    def _add_member_groups(self, tenant_uuid, wazo_user_id, username, sector_name):
+    def _add_member_groups(self, tenant_uuid, wazo_user_id, username, sector_name, job_id):
         # Add the Wazo user form Groups
         # Get sector group_id
-        group_sector_id = self._find_group(tenant_uuid, sector_name)
+        group_sector_id = self._find_group(tenant_uuid, sector_name + '_' + job_id)
         # Get user group_id
         group_user_id = self._find_group(tenant_uuid, username)       
         # Add Wazo user to group
@@ -126,7 +126,8 @@ class KoalaService(object):
                 tenant_uuid,
                 wazo_user_id,
                 params.get('username'),
-                params.get('sector')['name']
+                params.get('sector')['name'],
+                params.get('job')['id']
             )
             return True
         else:
